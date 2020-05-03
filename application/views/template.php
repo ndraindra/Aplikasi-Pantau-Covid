a7c7dcad40ba47b6984cd766fecb71219f0b55f1
ndraindra <dwiwijaya217@gmail.com>
2020-05-03 21:07:37 +0700

    pantau covid

---
+++ application/views/template.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  		<title><?php echo $title; ?> | Pantau Virus Corona (COVID-19)</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="<?php echo base_url('assets/'); ?>img/icon01.png" rel="icon">
+  <link href="<?php echo base_url('assets/'); ?>img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?php echo base_url('assets/'); ?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo base_url('assets/'); ?>vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="<?php echo base_url('assets/'); ?>vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?php echo base_url('assets/'); ?>vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="<?php echo base_url('assets/'); ?>vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="<?php echo base_url('assets/'); ?>vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="<?php echo base_url('assets/'); ?>css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Ninestars - v2.0.0
+  * Template URL: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container-fluid d-flex">
+
+      <div class="logo mr-auto">
+        <h1 class="text-light" style="color:#1d3661"><a href="<?php echo base_url(); ?>">PANTAU COVID-19</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="<?php echo base_url(); ?>">INDONESIA</a></li>
+          <li class="active"><a href="<?php echo base_url('global'); ?>">GLOBAL</a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1">
+          <h1>PANTAU CORONA VIRUS (COVID-19)</h1>
+          <h2>Update Data Corona Virus Indonesia & Data Covid-19 Dunia Terbaru <?php echo date('d M Y'); ?></h2>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img">
+          <img src="assets/img/illustration-7.png" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+
+   <?php echo $_content; ?>
+
+   <footer id="footer">
+
+    <div class="container py-4">
+      <div class="copyright">
+        &copy; <?php echo date('Y'); ?> Copyright <strong><span>Pantau Virus Corona (COVID-19) by Ndra Indra</span></strong>. <a href="https://kawalcorona.com/api/" target="_blank">Kawal Corona API</a> Made with <i class="fa fa-heart"></i> by <a href="https://teguh.co" target="_blank">Teguh Aprianto</a>
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/ -->
+        Template by <strong>Ninestars</strong>. Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer>
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="<?php echo base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo base_url('assets/'); ?>vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="<?php echo base_url('assets/'); ?>vendor/php-email-form/validate.js"></script>
+  <script src="<?php echo base_url('assets/'); ?>vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="<?php echo base_url('assets/'); ?>vendor/venobox/venobox.min.js"></script>
+  <script src="<?php echo base_url('assets/'); ?>vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="<?php echo base_url('assets/'); ?>vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="<?php echo base_url('assets/'); ?>js/main.js"></script>
+
+</body>
+
+</html>
